628335cf3f6259c1712381183c155ae6087fa8f6
anio7 <ydna.andy@hotmail.com>
2026-02-16 22:36:36 +0000

    learning

---
+++ day13/day13.py
@@ -0,0 +1,14 @@
+import random
+
+def add(n1,n2):
+    return n1 + n2
+
+def mutate(a_list):
+    b_list = []
+    new_item = 0
+    for item in a_list:
+        new_item = item * 2
+        new_item += random.randint(a=1, b=3)
+        new_item = add(new_item, item)
+    b_list.append(new_item)
+    print(b_list)
